4348acd98a316fca4ad91ecb0049a835817769f0
Jae Gifford <jae.giff@outlook.com>
2022-07-27 15:41:06 +0800

    tabbing between home and menu works

--- dist/pages/header.js
+++ dist/pages/header.js
@@ -11,16 +11,17 @@ const header = () => {
     const menuDiv = document.createElement('div');
     menuDiv.id = 'tab-menu';
     const homeBtn = document.createElement('button');
-    homeBtn.textContent = 'Home'
+    homeBtn.textContent = 'Home';
+    homeBtn.id = 'home-button';
     const menuBtn = document.createElement('button');
     menuBtn.textContent = 'Menu';
+    menuBtn.id = 'menu-button'
     const bookBtn = document.createElement('button');
     bookBtn.textContent = 'Book';
+    bookBtn.id = 'book-button';
     menuDiv.append(homeBtn, menuBtn, bookBtn);
 
-
     header.append(iconsDiv, menuDiv);
-
 
     return header;
 }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,44 @@ import footer from "../dist/pages/footer.js";
 import {eyeCatch, menu, wine} from "../dist/pages/menu.js";
 
 const content = document.getElementById('content');
-const menuContent = document.getElementById('menu-content');
 
-menuContent.append(header(), eyeCatch(), menu(), wine(), footer());
-content.append(header(), landing(), foodTypesContainer(), georgiaInfo(), footer());
+const removeChildren = () => {
+    const content = document.getElementById('content');
+    while (content.children.length > 0) {
+        content.children[0].remove();
+    };
+};
+
+const tabListeners = () => {
+    const menuBtn = document.getElementById('menu-button');
+    menuBtn.addEventListener('click', () => {
+        removeChildren();
+        loadMenu();
+    });
+
+    const homeBtn = document.getElementById('home-button');
+    homeBtn.addEventListener('click', () => {
+        removeChildren();
+        loadHome();
+        console.log('passed-hopme');
+    });
+};
+
+const loadHome = () => {
+    const homePage = document.createElement('div');
+    homePage.id = 'home-content';
+    content.appendChild(homePage);
+    homePage.append(header(), landing(), foodTypesContainer(), georgiaInfo(), footer());
+    tabListeners();
+};
+
+const loadMenu = () => {
+    const menuPage = document.createElement('div');
+    menuPage.id = 'menu-content';
+    content.appendChild(menuPage);
+    menuPage.append(header(), eyeCatch(), menu(), wine(), footer());
+    console.log('passed');
+    tabListeners();
+}
+
+loadHome();
