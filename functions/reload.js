c361994c31c8d37c8e27318d4126a5aec9374940
Jae Gifford <jae.giff@outlook.com>
2022-07-27 20:02:13 +0800

    refactored and reorganized again

--- functions/reload.js
+++
@@ -1,8 +0,0 @@
-const removeChildren = () => {
-    const content = document.getElementById('content');
-    while (content.children.length > 0) {
-        content.children[0].remove();
-    };
-};
-
-export {removeChildren};
